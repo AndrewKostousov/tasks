a45600b7f25f7ee572b66fa87439c792fad3951c
andrew <Andrew.Kostousov@gmail.com>
2016-06-13 12:59:11 +0500

    + JsonPathTest

---
+++ Core/JsonPathTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Core
+{
+    [TestFixture]
+    public class JsonPathTest
+    {
+        [Test]
+        public void Case1()
+        {
+            Assert.True(true);
+        }
+    }
+}
