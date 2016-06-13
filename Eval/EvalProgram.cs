beeb7ad87177b773e7d887b1a7c44f735bb673fa
andrew <Andrew.Kostousov@gmail.com>
2016-06-13 11:39:28 +0500

    eval using DataTable

--- Eval/EvalProgram.cs
+++ Eval/EvalProgram.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Data;
 
 namespace EvalTask
 {
-	class EvalProgram
-	{
-		static void Main(string[] args)
-		{
-			string input = Console.In.ReadToEnd();
-			string output = "TODO";
-			Console.WriteLine(output);
-		}
-	}
+    internal class EvalProgram
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.In.ReadToEnd();
+            var output = Evaluate(input).ToString();
+            Console.WriteLine(output);
+        }
+
+        public static double Evaluate(string expression)
+        {
+            var table = new DataTable();
+            table.Columns.Add("expression", typeof(string), expression);
+            var row = table.NewRow();
+            table.Rows.Add(row);
+            return double.Parse((string) row["expression"]);
+        }
+    }
 }
