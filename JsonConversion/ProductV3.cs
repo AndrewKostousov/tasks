e2f3c6611a4eb88960dd216eb6662dcdace0d459
kokovin <alexanderkokovin@gmail.com>
2016-06-13 14:37:59 +0500

    json3

--- JsonConversion/ProductV3.cs
+++ JsonConversion/ProductV3.cs
@@ -6,5 +6,13 @@
         public string name { get; set; }
         public decimal price { get; set; }
         public int count { get; set; }
+        public Dimensions dimensions { get; set; }
+    }
+
+    public class Dimensions
+    {
+        public int l { get; set; }
+        public int w { get; set; }
+        public int h { get; set; }
     }
 }
